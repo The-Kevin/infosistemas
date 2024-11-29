1192df3e0231cf50797738f9062cb16727e5d651
Kevin Almeida <kevin.almeida1111.ka@gmail.com>
2024-11-29 19:24:28 -0300

    feat: add validations in brand DTOs

--- src/brands/dtos/brand.dto.ts
+++ src/brands/dtos/brand.dto.ts
@@ -1,7 +1,10 @@
 import IGenericModel from 'src/utils/interfaces/genericModel.interface';
 import { IBrand } from '../interfaces/brand.interface';
+import { ArrayNotEmpty, IsUUID, MaxLength } from 'class-validator';
+import { Type } from 'class-transformer';
 
 export class CreateBrandDTO {
+  @MaxLength(255)
   name: string;
 }
 
@@ -10,10 +13,13 @@ export interface ReturnCreateBrandDTO extends IGenericModel {
 }
 
 export class UpdateBrandDTO {
+  @IsUUID()
   id: string;
+  @Type(() => IBrand)
   data: Partial<IBrand>;
 }
 
 export class DeleteBrandDTO {
+  @ArrayNotEmpty()
   ids: string[];
 }
